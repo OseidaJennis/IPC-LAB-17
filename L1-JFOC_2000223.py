c8c4b328f27f4e43149e791e69cb6422cbc53db0
OseidaJennis <144158476+OseidaJennis@users.noreply.github.com>
2023-09-05 21:49:06 -0600

    Add files via upload

---
+++ L1-JFOC_2000223.py
@@ -0,0 +1,72 @@
+#Introducción a la programacón - Sección 17
+# Fecha de creación del programa: 5/9/2023
+#Autor: Jennifer Fabiola Oseida Castillo
+# Objetivo: Crear un programa en python que calcule 4 operaciones aritméticas básicas 
+#Entrada: 2 números enteros n1, n2
+"""
+Procesos principales:
+1. Solicitar al usuario 2 números enteros 
+2. Realizar los cálculos de suma, resta, multipkicación, división 
+3. Mostrar los resultados
+"""
+print("Programa para realizar cálculos matemáticos")
+#Salida
+print("Operación Suma")
+#Establecer variables
+N1=1
+N2=2
+
+#Solicitar datos
+N1= int(input("Ingrese el primer número:"))
+N2= int(input("Ingrese el segundo número:"))
+
+#Operaciones y resultados
+Suma= N1+N2
+
+#Mostrar resultados
+print("El resultado de la suma es:", Suma)
+
+print("Operación Resta")
+#Establecer variables
+N1=1
+N2=2
+
+#Solicitar datos
+N1= int(input("Ingrese el primer número:"))
+N2= int(input("Ingrese el segundo número:"))
+
+#Operaciones y resultados
+Resta= N1-N2
+
+#Mostrar resultados
+print("El resultado de la resta es:", Resta)
+
+print("Operación Multiplicación")
+#Establecer variables
+N1=1
+N2=2
+
+#Solicitar datos
+N1= int(input("Ingrese el primer número:"))
+N2= int(input("Ingrese el segundo número:"))
+
+#Operaciones y resultados
+Multiplicación= N1*N2
+
+#Mostrar resultados
+print("El resultado de la multiplicación es:", Multiplicación)
+
+print("Operación División")
+#Establecer variables
+N1=1
+N2=2
+
+#Solicitar datos
+N1= int(input("Ingrese el primer número:"))
+N2= int(input("Ingrese el segundo número:"))
+
+#Operaciones y resultados
+División= N1/N2
+
+#Mostrar resultados
+print("El resultado de la resta es:", División)
