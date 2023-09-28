c0eaa315b9da80e48d4fdda9e9af9782f37dd4c8
OseidaJennis <144158476+OseidaJennis@users.noreply.github.com>
2023-09-28 09:59:26 -0600

    Update TS8-JFOC_2000223.py

--- TS8-JFOC_2000223.py
+++ TS8-JFOC_2000223.py
@@ -5,12 +5,12 @@
 #Entrada: Mostrar la secuencia de numeros en distinto orden
 """"
 Proceso principal
-1. Solicitiar al usuario que ingrese un número del apartado menú 
-2. Ejecutar el programa
-    Si selecciona opción 1 mostrará una solución factorial
-    Si selecciona opción 2 mostrará la secuencia de fibonacci del número ingresado
-3. Mostrar el resultado en pantalla
-4. Si selecciona opción 3 saldrá del programa
+1. Secuencia de numeros en un rango de 1- 25, incrementando de 1 en 1 utilizando FOR
+2. Secuencia de numeros en un rango de 1 - 25, incrementando de 1 en 1 utilizando WHILE
+3. Secuencia de numeros en un rango de 5 - 50, incrementando de 5 en 5 utilizando FOR
+4. Secuencia de numeros en un rango de 5 - 50, incrementando de 5 en 5 utilizando WHILW
+5. Secuencia de numeros en un rango de 20 - 0, decrementando de 2 en 2 utilizando FOR
+6. Secuencia de numeros en un rango de 20 - 0, decrementando de 2 en 2 utilizando WHILE
 """
 print("Uso ciclos FOR Y WHILE")
 #Salida
